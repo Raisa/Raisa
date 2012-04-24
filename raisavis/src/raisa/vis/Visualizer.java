0ebbbbdf1745a3f6422bb6ec5287c2db3a95edf4
Markku Rontu <markku.rontu@iki.fi>
2012-04-24 23:17:21 +0300

    Added measure line

--- raisavis/src/raisa/vis/Visualizer.java
+++ raisavis/src/raisa/vis/Visualizer.java
@@ -2,20 +2,33 @@ package raisa.vis;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Visualizer extends JPanel {
+    private float mx;
+    private float my;
     public List<Spot> spots;
     
     public Visualizer() {
-        setBackground(Color.black);
+        setBackground(Color.gray);
+        addMouseMotionListener(new MouseAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent mouseEvent) {
+                mx = mouseEvent.getX();
+                my = mouseEvent.getY();
+                repaint();
+            }
+        });
     }
     
     public void paintComponent(Graphics g) {
+        g.clearRect(0, 0, getBounds().width, getBounds().height);
         float cx = getBounds().width * 0.5f;
         float cy = getBounds().height * 0.5f;
-        setForeground(Color.gray);
+        g.setColor(Color.black);
         for (Spot spot : spots) {
             float w = 3.0f;
             float h = 3.0f;
@@ -23,6 +36,12 @@ public class Visualizer extends JPanel {
             float y = cy + spot.y - 0.5f * h;
             g.fillRect((int) x, (int) y, (int) w, (int) h);
         }
+        g.setColor(Color.blue);
+        g.drawLine((int)cx, (int)cy, (int)mx, (int)my);
+        float dx = cx - mx;
+        float dy = cy - my;
+        float l = (float)Math.sqrt(dx * dx + dy * dy);
+        g.drawString(l + " cm",  0, 20);
     }
     
     public static void main(String[] args) {
