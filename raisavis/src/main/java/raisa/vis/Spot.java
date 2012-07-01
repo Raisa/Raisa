abec46f64a0ec5f162f79fa3b495cf189d3e9011
Markku Rontu <markku.rontu@iki.fi>
2012-07-01 19:13:46 +0300

    Refactored floats to Floats (vectors)

--- raisavis/src/main/java/raisa/vis/Spot.java
+++ raisavis/src/main/java/raisa/vis/Spot.java
@@ -1,10 +1,11 @@
 package raisa.vis;
 
-public class Spot {
-    public float x;
-    public float y;
-    public Spot(float x, float y) {
-        this.x = x;
-        this.y = y;
-    }
+import java.awt.geom.Point2D;
+
+public class Spot extends Point2D.Float {
+	private static final long serialVersionUID = 1L;
+
+	public Spot(float x, float y) {
+		super(x, y);
+	}
 }
--- raisavis/src/main/java/raisa/vis/VisualizerPanel.java
+++ raisavis/src/main/java/raisa/vis/VisualizerPanel.java
@@ -1,5 +1,7 @@
 package raisa.vis;
 
+import static java.awt.geom.Point2D.Float;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.HierarchyBoundsListener;
@@ -8,99 +10,27 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class VisualizerPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
-	private float rx;
-	private float ry;
-	private float cx;
-	private float cy;
-	private float mx;
-	private float my;
-	private float mdx;
-	private float mdy;
+	private Float robot = new Float();
+	private Float camera = new Float();
+	private Float mouse = new Float();
+	private Float mouseDragStart = new Float();
 	float scale = 2.0f;
 	public List<Spot> spots;
 
 	public VisualizerPanel() {
 		setBackground(Color.gray);
 		setFocusable(true);
-		addHierarchyBoundsListener(new HierarchyBoundsListener() {
-			@Override
-			public void ancestorResized(HierarchyEvent arg0) {
-				// cx = getBounds().width * 0.5f;
-				// cy = getBounds().height * 0.5f;
-				resetMouseLocationLine();
-				repaint();
-			}
-
-			private void resetMouseLocationLine() {
-				mx = cx;
-				my = cy;
-			}
-
-			@Override
-			public void ancestorMoved(HierarchyEvent arg0) {
-			}
-		});
-		addMouseMotionListener(new MouseAdapter() {
-			@Override
-			public void mouseMoved(MouseEvent mouseEvent) {
-				mx = mouseEvent.getX();
-				my = mouseEvent.getY();
-				repaint();
-			}
-
-			@Override
-			public void mouseDragged(MouseEvent mouseEvent) {
-				my = mouseEvent.getY();
-				mx = mouseEvent.getX();
-				cx += (mdx - mx) / scale;
-				cy += (mdy - my) / scale;
-				mdx = mx;
-				mdy = my;
-				repaint();
-			}
-		});
-		addMouseListener(new MouseAdapter() {
-			@Override
-			public void mousePressed(MouseEvent mouseEvent) {
-				mdx = mouseEvent.getX();
-				mdy = mouseEvent.getY();
-			}			
-		});
-		addKeyListener(new KeyAdapter() {
-			@Override
-			public void keyReleased(KeyEvent arg0) {
-				int keyCode = arg0.getKeyCode();
-				if (keyCode == KeyEvent.VK_C) {
-					System.out.println("Cleared");
-					spots.clear();
-					repaint();
-				} else if (keyCode == KeyEvent.VK_H) {
-					System.out.println("Removed old points");
-					int fromIndex = Math.max(0, spots.size() - 1000);
-					int toIndex = spots.size();
-					List<Spot> newSpots = new ArrayList<Spot>();
-					newSpots.addAll(spots.subList(fromIndex, toIndex));
-					spots = newSpots;
-					repaint();
-				} else if (keyCode == KeyEvent.VK_PLUS) {
-					scale *= 1.25f;
-					repaint();
-				} else if (keyCode == KeyEvent.VK_MINUS) {
-					scale *= 0.8f;
-					repaint();
-				}
-			}
-		});
+		addHierarchyBoundsListener(new PanelSizeHandler());
+		addMouseMotionListener(new MouseMotionHandler());
+		addMouseListener(new MouseHandler());
+		addKeyListener(new KeyboardHandler());
 	}
 
 	public void update(List<Spot> arg) {
@@ -112,38 +42,121 @@ public class VisualizerPanel extends JPanel {
 		int screenWidth = getBounds().width;
 		int screenHeight = getBounds().height;
 		g.clearRect(0, 0, screenWidth, screenHeight);
+		
 		g.setColor(Color.black);
 		for (Spot spot : spots) {
-			float w = 3.0f;
-			float h = 3.0f;
-			float x = 0.5f * screenWidth + scale * (spot.x - cx) - 0.5f * w;
-			float y = 0.5f * screenHeight + scale * (spot.y - cy) - 0.5f * h;
-			g.fillRect((int) x, (int) y, (int) w, (int) h);
+			drawPoint(g, spot);
 		}
 
 		g.setColor(Color.blue);
-		float x = (rx - cx) * scale + 0.5f * screenWidth;
-		float y = (ry - cy) * scale + 0.5f * screenHeight;
-		g.drawLine((int) x, (int) y, (int) mx, (int) my);
-		float dx = (x - mx) / scale;
-		float dy = (y - my) / scale;
+		drawMeasurementLine(g, robot, toWorld(mouse));
+		
+		Spot latestSpot = spots.get(spots.size() - 1);
+		g.setColor(Color.red);
+		drawMeasurementLine(g, robot, latestSpot);
+	}
+
+	private void drawPoint(Graphics g, Float point) {
+		float w = 3.0f;
+		float h = 3.0f;
+		Float p = toScreen(point);
+		g.fillRect((int) (p.x - 0.5f * w), (int) (p.y - 0.5f * h), (int) w, (int) h);
+	}
+
+	private void drawMeasurementLine(Graphics g, Float from, Float to) {
+		Float p1 = toScreen(from);
+		Float p2 = toScreen(to);
+		g.drawLine((int)p1.x, (int)p1.y, (int)p2.x, (int)p2.y);
+		float dx = (from.x - to.x);
+		float dy = (from.y - to.y);
 		float l = (float) Math.sqrt(dx * dx + dy * dy);
 		String distanceString = String.format("%3.1f cm", l);
-		g.drawString(distanceString, (int) (mx + 10), (int) my);
-
-		float sx = spots.get(spots.size() - 1).x;
-		float sy = spots.get(spots.size() - 1).y;
-
-		g.setColor(Color.red);
-		x = (rx - cx) * scale + 0.5f * screenWidth;
-		y = (ry - cy) * scale + 0.5f * screenHeight;
-		float lx = (sx - cx) * scale + 0.5f * screenWidth;
-		float ly = (sy - cy) * scale + 0.5f * screenHeight;
-		g.drawLine((int) x, (int) y, (int) lx, (int) ly);
-		dx = (x - lx) / scale;
-		dy = (y - ly) / scale;
-		l = (float) Math.sqrt(dx * dx + dy * dy);
-		distanceString = String.format("%3.1f cm", l);
-		g.drawString(distanceString, (int) (lx + 10), (int) ly);
+		g.drawString(distanceString, (int)(p2.x + 10), (int)p2.y);
 	}
+
+	private Float toWorld(Float screen) {
+		int screenWidth = getBounds().width;
+		int screenHeight = getBounds().height;
+		return new Float(camera.x + (screen.x - screenWidth * 0.5f) / scale, camera.y + (screen.y - screenHeight * 0.5f) / scale);
+	}
+
+	private Float toScreen(Float from) {
+		int screenWidth = getBounds().width;
+		int screenHeight = getBounds().height;
+		float x1 = (from.x - camera.x) * scale + 0.5f * screenWidth;
+		float y1 = (from.y - camera.y) * scale + 0.5f * screenHeight;
+		Float f = new Float(x1, y1);
+		return f;
+	}
+	
+	private final class PanelSizeHandler implements HierarchyBoundsListener {
+		@Override
+		public void ancestorResized(HierarchyEvent arg0) {
+			resetMouseLocationLine();
+			repaint();
+		}
+
+		private void resetMouseLocationLine() {
+			mouse.x = camera.x;
+			mouse.y = camera.y;
+		}
+
+		@Override
+		public void ancestorMoved(HierarchyEvent arg0) {
+		}
+	}
+
+	private final class MouseMotionHandler extends MouseAdapter {
+		@Override
+		public void mouseMoved(MouseEvent mouseEvent) {
+			mouse.x = mouseEvent.getX();
+			mouse.y = mouseEvent.getY();
+			repaint();
+		}
+
+		@Override
+		public void mouseDragged(MouseEvent mouseEvent) {
+			mouse.x = mouseEvent.getX();
+			mouse.y = mouseEvent.getY();
+			camera.x += (mouseDragStart.x - mouse.x) / scale;
+			camera.y += (mouseDragStart.y - mouse.y) / scale;
+			mouseDragStart.x = mouse.x;
+			mouseDragStart.y = mouse.y;
+			repaint();
+		}
+	}
+
+	private final class MouseHandler extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent mouseEvent) {
+			mouseDragStart.x = mouseEvent.getX();
+			mouseDragStart.y = mouseEvent.getY();
+		}
+	}
+
+	private final class KeyboardHandler extends KeyAdapter {
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			int keyCode = arg0.getKeyCode();
+			if (keyCode == KeyEvent.VK_C) {
+				System.out.println("Cleared");
+				spots.clear();
+				repaint();
+			} else if (keyCode == KeyEvent.VK_H) {
+				System.out.println("Removed old points");
+				int fromIndex = Math.max(0, spots.size() - 1000);
+				int toIndex = spots.size();
+				List<Spot> newSpots = new ArrayList<Spot>();
+				newSpots.addAll(spots.subList(fromIndex, toIndex));
+				spots = newSpots;
+				repaint();
+			} else if (keyCode == KeyEvent.VK_PLUS) {
+				scale *= 1.25f;
+				repaint();
+			} else if (keyCode == KeyEvent.VK_MINUS) {
+				scale *= 0.8f;
+				repaint();
+			}
+		}
+	}	
 }
