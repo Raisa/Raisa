1c61007aa4d946fd21fd3a4665549968934bb015
eputtone <esa.puttonen@gmail.com>
2014-05-16 17:58:28 +0300

    Enabled scanning in every simulator tick (RotatingServo)

--- raisavis/src/main/java/raisa/simulator/RotatingServo.java
+++ raisavis/src/main/java/raisa/simulator/RotatingServo.java
@@ -3,17 +3,14 @@ package raisa.simulator;
 
 public class RotatingServo {
 	private float heading;
-	private float rotatingSpeed = 60.0f / 0.17f;
-	private float minHeading = -90;
-	private float maxHeading = 90;
+	private final float rotatingSpeed = 60.0f / 0.17f;
+	private final float minHeading = -90;
+	private final float maxHeading = 90;
 	private static final int LEFT = -1;
 	private static final int RIGHT = +1;
-	private float scanOffset = 0;
-	private float scanStep = 20;
-	private float nextScanHeading = 10;
 	private int direction = RIGHT;
-	private ServoScanListener servoScanListener;
-	
+	private final ServoScanListener servoScanListener;
+
 	public RotatingServo(ServoScanListener servoScanListener) {
 		this.heading = (minHeading + maxHeading) / 2.0f;
 		this.servoScanListener = servoScanListener;
@@ -21,37 +18,14 @@ public class RotatingServo {
 
 	public RotatingServo rotate(float timestep) {
 		heading += direction * timestep * rotatingSpeed;
+		scan(heading);
 		int nextDirection = direction;
-		if (direction == LEFT && heading < nextScanHeading) {
-			heading = nextScanHeading;
-			scan(heading);
-			nextScanHeading -= scanStep;
-		}
-		if (direction == RIGHT && heading > nextScanHeading) {
-			heading = nextScanHeading;
-			scan(heading);
-			nextScanHeading += scanStep;
-		}
 		if (heading >= maxHeading) {
-			heading = maxHeading;
-			scan(heading);
 			nextDirection = LEFT;
-			nextScanHeading = maxHeading - scanStep - scanOffset;
-			scanOffset++;
-			if (scanOffset > scanStep) {
-				scanOffset = 0;
-			}
 		}
-
 		if (heading <= minHeading) {
-			heading = minHeading;
 			scan(heading);
 			nextDirection = RIGHT;
-			nextScanHeading = minHeading + scanStep + scanOffset;
-			scanOffset++;
-			if (scanOffset > scanStep) {
-				scanOffset = 0;
-			}
 		}
 		direction = nextDirection;
 		return this;
